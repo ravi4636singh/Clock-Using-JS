bd3b8e85834cd1b20360980ff3d18fcef4ab166f
Ravi Singh <ravi4636singh@gmail.com>
2023-04-10 21:54:46 +0530

    Add enter name screen and that name show on main page

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
+const form = document.querySelector('#form_input')
+const personName = document.querySelector('#name')
+let displayName = ''
+
 const clock = () => {
     const dt = new Date()
 
     let hours = dt.getHours().toLocaleString().padStart(2, '0')
-    document.querySelector('.hours').textContent = hours
-
     let minutes = dt.getMinutes().toLocaleString().padStart(2, '0')
-    document.querySelector('.minutes').textContent = minutes
-    
     let seconds = dt.getSeconds().toLocaleString().padStart(2, '0')
+    let greetingMsg = ''
+
+    if(hours >= 0 && hours < 12){
+        greetingMsg = 'Good Morning'
+    }else if(hours >= 12 && hours < 16){
+        greetingMsg = 'Good Afternoon'
+    }else{
+        greetingMsg = 'Good Evening'
+    }
+    
+    document.querySelector('.greeting').textContent = `${greetingMsg}, ${displayName}`
+    document.querySelector('.hours').textContent = hours
+    document.querySelector('.minutes').textContent = minutes
     document.querySelector('.seconds').innerHTML = seconds
+    
+    setInterval(clock, 1000)
 }
 
-setInterval(clock, 1000)
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if(personName === ''){
+        alert('Please! Enter Name')
+    }else{
+        displayName = personName.value
+        clock()
+        document.querySelector('.form').style.display = 'none'
+        document.querySelector('.main-screen').style.display = 'block'
+    }
+})
